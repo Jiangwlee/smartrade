c1565a354b7ef0dba3d07ea210cdde2535df1263
Bruce Li <jiangwlee@163.com>
2024-09-10 14:28:14 +0800

    添加涨停板爬虫

--- crawlers/src/crawlers/ths/dto.py
+++ crawlers/src/crawlers/ths/dto.py
@@ -22,7 +22,7 @@ Version:
 """
 
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
 
 """
 每日涨跌停汇总.
@@ -40,7 +40,7 @@ class StockDayOverDaySummary(BaseModel):
     today: StockDailySummary
     yesterday: StockDailySummary
     
-class StockModel(BaseModel):
+class LimitDownStockModel(BaseModel):
     code: str = Field(..., description="股票代码")
     name: str = Field(..., description="股票名称")
     change_rate: float = Field(..., description="涨幅")
@@ -56,9 +56,34 @@ class PageInfo(BaseModel):
     count: int = Field(..., description="总页数")
     page: int = Field(..., description="当前是第几页")
 
-class RespDataModel(BaseModel):
+class LimitDownRespDataModel(BaseModel):
     page: PageInfo
-    info: List[StockModel]
+    info: List[LimitDownStockModel]
+    limit_up_count: StockDayOverDaySummary
+    limit_down_count: StockDayOverDaySummary
+    date: str
+
+
+class LimitUpStockModel(BaseModel):
+    code: str = Field(..., description="股票代码")
+    name: str = Field(..., description="股票名称")
+    change_rate: float = Field(..., description="涨幅")
+    last_limit_up_time: str = Field(..., description="最后一次涨停时间")
+    first_limit_up_time: str = Field(..., description="首次涨停时间")
+    turnover_rate: float = Field(..., description="换手率")
+    market_type: str = Field(..., description="市场类型, HS 代表沪深")
+    currency_value: int = Field(..., description="流通市值")
+    open_num: Optional[int] = Field(..., description="打开涨停次数")
+    limit_up_type: str = Field(..., description="涨停板类型")
+    order_volume: int = Field(..., description="封单量")
+    order_amount: float = Field(..., description="封单金额")
+    limit_up_suc_rate: Optional[float] = Field(..., description="封板成功率")
+    reason_type: Optional[str] = Field(..., description="涨停原因")
+    high_days: str = Field(..., description="几天几板")
+
+class LimitUpRespDataModel(BaseModel):
+    page: PageInfo
+    info: List[LimitUpStockModel]
     limit_up_count: StockDayOverDaySummary
     limit_down_count: StockDayOverDaySummary
     date: str
---
+++ crawlers/src/crawlers/ths/fieldtype.py
@@ -0,0 +1,40 @@
+"""
+fieldtype
+=============
+
+同花顺的API参数.
+
+Functions:
+-----------
+TBD
+
+Usage:
+------
+TBD
+
+Author:
+-------
+Bruce Li (jiangwlee@163.com)
+
+Version:
+--------
+0.0.1
+"""
+
+from enum import Enum
+
+# 定义参数类型
+class FieldType(Enum):
+	CHANGE_RATE = 199112,  # 涨幅
+	LATEST = 10,  # 最新价格
+	REASON_TYPE = 9001,  # 涨停原因
+	FIRST_LIMIT_UP_TIME = 330323,  # 首次涨停时间
+	LAST_LIMIT_UP_TIME = 330324,  # 最终涨停时间
+	LIMIT_UP_TYPE = 330325,  # 涨停类型
+	OPEN_NUM = 9002,  # 打开次数
+	HIGH_DAYS_VALUE = 330329,  # 几天几板
+	ORDER_VOLUME = 133971,  # 封单量
+	ORDER_AMOUNT = 133970,  # 封单金额
+	TURNOVER_RATE = 1968584,  # 换手率
+	CURRENCY_VALUE = 3475914,  # 流通市值
+	LIMIT_UP_SUC_RATE = 9003,  # 开板次数
