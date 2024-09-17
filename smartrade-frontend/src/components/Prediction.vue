0f15ce7999674f1780992c97656f50a125170750
李江维 <179887890@qq.com>
2024-09-17 21:47:01 +0800

    Sort result

--- smartrade-frontend/src/components/Prediction.vue
+++ smartrade-frontend/src/components/Prediction.vue
@@ -1,4 +1,19 @@
 <template>
+  <div class="block">
+    <span class="demonstration">Picker with quick options</span>
+    <el-date-picker
+      v-model="pickedDate"
+      type="date"
+      format="YYYY-MM-DD"
+      placeholder="Pick a day"
+      :disabled-date="disabledDate"
+      :shortcuts="shortcuts"
+      size="small"
+    />
+
+    <el-button type="primary" size="small" :onclick="fetchPredictions">Primary</el-button>
+  </div>
+
   <el-table :data="tableData" style="width: 100%">
     <el-table-column prop="date" label="日期" />
     <el-table-column prop="code" label="股票代码"/>
@@ -9,20 +24,61 @@
 </template>
   
 <script lang="ts" setup>
-  import { reactive, onMounted } from 'vue';
+  import { ref, reactive, onMounted } from 'vue';
   import { getPrediction } from '../services/requests'
   import { type Prediction } from '@/services/types';
 
+  const pickedDate = ref('')
   const tableData = reactive<Prediction[]>([])
   
   const fetchPredictions = async () => {
-    const resp = await getPrediction('20240913');
+    const resp = await getPrediction(formatDate(pickedDate.value));
     console.log(resp.data.data)
     tableData.length = 0;
     tableData.push(...resp.data.data)
   };
 
+  const disabledDate = (time: Date) => {
+    return time.getTime() > Date.now()
+  }
+
+  const shortcuts = [
+    {
+      text: 'Today',
+      value: new Date(),
+    },
+    {
+      text: 'Yesterday',
+      value: () => {
+        const date = new Date()
+        date.setTime(date.getTime() - 3600 * 1000 * 24)
+        return date
+      },
+    },
+    {
+      text: 'A week ago',
+      value: () => {
+        const date = new Date()
+        date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+        return date
+      },
+    },
+  ]
+
+  const formatDate = (value: string) => {
+    console.log("Date: " + value)
+    if (value) {
+        const date = new Date(value);
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}${month}${day}`;
+      } else {
+        return '';
+      }
+  }
+
   onMounted(() => {
-    fetchPredictions();
+    // fetchPredictions();
   });
 </script>
